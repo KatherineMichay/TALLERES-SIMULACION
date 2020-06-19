82c9effaa1d4486a64306a64ad6b5ae461ab8d19
Stefano <sebastiancruz1996@gmail.com>
2020-06-18 19:51:58 -0500

    HONESTAMENTE NO SE QUE SON :V

--- Taller 3/TallerA.R/TallerA.R/Material/instalacion.paquetes.R
+++
@@ -1,6 +0,0 @@
-# Para instalar los paquetes empleados basta con ejecutar en la consola de R:
-
-pkgs <- c('Rcmdr','MASS','DEoptim','nortest','tseries','geoR','copula')
-# install.packages(pkgs, dependencies=TRUE)
-install.packages(setdiff(pkgs, installed.packages()[,"Package"]), dependencies = TRUE)
-
